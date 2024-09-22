a3ef58defeb28850c57a74656356f4bcc56d6dea
faizul726 <162413089+faizul726@users.noreply.github.com>
2024-09-22 15:04:30 +0600

    wip v1.1

--- restoreVanillaShaders.bat
+++ restoreVanillaShaders.bat
@@ -2,48 +2,184 @@
 setlocal enabledelayedexpansion
 
 cd %~dp0
-
+cls
 if not defined mcLocation ( for /f "tokens=*" %%i in ('powershell -command "Get-AppxPackage -Name Microsoft.MinecraftUWP | Select-Object -ExpandProperty InstallLocation"') do set "mcLocation=%%i" )
-if not exist "tmp\" mkdir tmp
+if not defined mcLocation (
+    echo [41;97m[^^!] Couldn't find Minecraft installation location.[0m
+    echo.
+    pause
+    goto:EOF
+)
+
+if exist "tmp\" (
+    rmdir /s /q tmp
+    mkdir "tmp"
+) else (
+    mkdir "tmp"
+)
 
 if "%restoreType%" equ "full" (
+    echo [93m[*] Restore type: Full[0m && echo.
     goto fullRestore
 ) else if "%restoreType%"=="partial" (
     goto partialRestore
 )
 
-echo [93mHow would you like to restore?[0m
-echo %restoreType%
+if not exist "materials.bak\" (
+    echo [41;97mNo previous backup found.[0m && echo.
+    pause
+    goto:EOF
+)
+
+set rstrCount=
+for %%f in ("materials.bak\*") do set /a rstrCount+=1
+if not defined rstrCount (
+    echo [41;97mmaterials.bak folder is empty.[0m && echo.
+    pause
+    goto:EOF
+)
+
+echo [93m[?] How would you like to restore?[0m
 echo.
 echo [1] Full restore (restore all materials)
 echo [2] Partial restore (only restore the ones modified in previous injection)
 echo [3] Exit
 echo.
 choice /c 123 /n 
-if %errorlevel% equ 1 goto fullRestore
-if %errorlevel% equ 2 goto partialRestore
-if %errorlevel% equ 3 goto:EOF
-
+if !errorlevel! equ 1 (
+    cls
+    echo [93m[*] Restore type: Full[0m && echo.
+    goto fullRestore
+)
+if !errorlevel! equ 2 (
+    cls
+    echo [93m[*] Restore type: Partial[0m && echo.
+    echo WORK IN PROGRESS
+    pause && goto:EOF
+    goto partialRestore
+)
+if !errorlevel! equ 3 goto:EOF
 
 :fullRestore
+set splitCount=
+for %%f in ("materials.bak\*") do (
+    set /a splitCount+=1
+    if !splitCount! leq 20 (
+        move /y "%%f" "tmp\" > NUL
+    )
+)
+if defined splitCount (
+    set /a stepCount+=1
+    goto fullRestore
+) else (
+    move "tmp\*" "materials.bak" > NUL
+    set /a stepCount=stepCount+2
+)
+
+echo [92mFound !rstrCount! materials(s).[0m
+
+echo [91m[^^!] Please accept all UAC prompts or it will fail.[0m && echo. && echo.
+
+timeout 5 > NUL && cls
+
+echo [93m[*] Running step 1/%stepCount%: Deleting game materials...[0m && echo.
+
+:fr-delete
+if exist "!mcLocation!\data\renderer\materials\" (
+    "%ProgramFiles(x86)%\IObit\IObit Unlocker\IObitUnlocker" /advanced /delete "!mcLocation!\data\renderer\materials"
+) else (
+    echo [92m[*] Done[0m && echo. && echo.
+    echo [93m[*] Running step 2/%stepCount%: Creating materials folder...[0m && echo.
+    if not exist "tmp\materials" mkdir "tmp\materials"
+    goto fr-mkdir
+)
 cls
-echo hi this is still WIP.
-pause
-goto:EOF
+if !errorlevel! neq 0 (
+    echo [93m[*] Running step 1/%stepCount%: Deleting game materials...[0m && echo.
+    echo [41;97mPlease accept UAC.[0m
+    echo.
+    echo [93mTrying again...[0m
+    echo.
+    goto fr-delete
+)
+
+
+:fr-mkdir
+"%ProgramFiles(x86)%\IObit\IObit Unlocker\IObitUnlocker" /advanced /move "%cd%\tmp\materials\" "!mcLocation!\data\renderer\"
+
+if !errorlevel! neq 0 (
+    cls
+    echo [93m[*] Running step 2/%stepCount%: Creating materials folder...[0m && echo.
+    echo [41;97mPlease accept UAC.[0m
+    echo.
+    echo [93mTrying again...[0m
+    echo.
+    goto fr-mkdir
+) 
+
+echo [92m[*] Done[0m && echo. && echo. 
+timeout 3 > NUL
+set currentStep=3
+
+:fr-split
+cls
+echo [93m[*] Running step %currentStep%/%stepCount%: Moving materials...[0m && echo.
+set splitCount=
+for %%f in ("materials.bak\*") do (
+    set /a splitCount+=1
+    if !splitCount! leq 20 (
+        move /y "%%f" "tmp\" > NUL
+    )
+)
+if not defined splitCount (
+    if exist "materials.bak\" (
+        rmdir /s /q materials.bak
+    )
+    if exist "tmp\" (
+        rmdir /s /q "tmp"
+    )
+    goto completed
+)
+
+:fr-move
+set srcList=
+for %%f in (tmp\*) do (
+    set srcList=!srcList!,"%cd%\%%f"
+    echo [93m[Moving] [0m%%~nxf
+)
+if defined srcList set "srcList=%srcList:~1%" 
+
+"%ProgramFiles(x86)%\IObit\IObit Unlocker\IObitUnlocker" /advanced /move !srcList! "!mcLocation!\data\renderer\materials"
+if !errorlevel! equ 0 (
+    cls
+    set /a currentStep+=1
+    goto fr-split
+) else (
+    cls
+    echo [93m[*] Running step %currentStep%/%stepCount%: Moving materials...[0m && echo.
+    echo [41;97mPlease accept UAC.[0m
+    echo.
+    echo [93mTrying again...[0m
+    echo.
+    goto fr-move
+)
 
 :partialRestore
-echo TESTING
+echo WIP && pause && goto:EOF
 if exist ".settings\.replaceList.log" (
     move ".settings\*.log" "tmp\"
     set /p bins=< "tmp\.bins.log"
     set /p srcList=< "tmp\.srcList.log"
     set /p replaceList=< "tmp\.replaceList.log"
     goto restore1
-) else echo [41;97mNo logs found for previous injection.[0m
+) else (
+    echo [41;97mNo logs found for previous injection.[0m
+)
+
 
 :restore1
 "%ProgramFiles(x86)%\IObit\IObit Unlocker\IObitUnlocker" /advanced /delete %replaceList%
-if not %errorlevel% equ 0 (
+if not !errorlevel! equ 0 (
     echo [41;97mPlease accept UAC next time^^![0m
     echo.
     pause
@@ -52,13 +188,13 @@ if not %errorlevel% equ 0 (
 ) else (
     echo [92mStep 1/2 succeed^^![0m
 )
-echo.
-echo.
 
+echo.
+echo.
 
 :restore2
 "%ProgramFiles(x86)%\IObit\IObit Unlocker\IObitUnlocker" /advanced /move !srcList! "!mcLocation!\data\renderer\materials"
-if not %errorlevel% equ 0 (
+if not !errorlevel! equ 0 (
     echo [41;97mPlease accept UAC next time^^![0m
     echo.
     pause
@@ -70,3 +206,8 @@ if not %errorlevel% equ 0 (
     goto: EOF
 )
 
+:completed
+cls
+echo [92m[*] Done[0m
+pause
+goto:EOF
--- settings.bat
+++ settings.bat
@@ -7,8 +7,9 @@ set "skipIntro=.settings\.skipIntroduction.txt"
 set "skipConfirmation=.settings\.skipConfirmation.txt"
 set "useAutoAlways=.settings\.useAutoAlways.txt"
 set "useManualAlways=.settings\.useManualAlways.txt"
+set "disableCooldown=.settings\.disableCooldown.txt"
 
-title Settings for Material Replacer
+title Settings for Matject
 
 :SETTINGS
 if not exist ".settings\" mkdir .settings
@@ -18,20 +19,22 @@ if exist "%skipIntro%" (set toggle1=%on%) else (set toggle1=%off%)
 if exist "%skipConfirmation%" (set toggle2=%on%) else (set toggle2=%off%)
 if exist "%useAutoAlways%" (set toggle3=%on%) else (set toggle3=%off%)
 if exist "%useManualAlways%" (set toggle4=%on%) else (set toggle4=%off%)
+if exist "%disableCooldown%" (set toggle5=%on%) else (set toggle5=%off%)
 
-echo You can change options of Material Replacer here.
+echo You can change options of Matject here.
 echo.
 
 echo %toggle1% 1. Skip introduction
 echo %toggle2% 2. Skip confirmation
 echo %toggle3% 3. Always use auto method
 echo %toggle4% 4. Always use manual method
+echo %toggle5% 5. Disable cooldown
 echo.
-echo 5. Exit
+echo B. Exit
 echo.
 
 echo Press number to toggle desired settings. [Press O to turn on all, F to turn off all]
-choice /c 1234of5 /n
+choice /c 12345ofb /n
 
 if %errorlevel% equ 1 (
 	if "%toggle1%"=="%off%" (
@@ -68,21 +71,28 @@ if %errorlevel% equ 4 (
 	) else del "%useManualAlways%"
 )
 
-
-
 if %errorlevel% equ 5 (
-	echo 5
-	echo 1 > "%skipIntro%"
-	echo 1 > "%skipConfirmation%"
+	if "%toggle5%"=="%off%" (
+		echo 1 > "%disableCooldown%"
+		) else (
+			del "%disableCooldown%"
+)
 )
 
 if %errorlevel% equ 6 (
+	echo 1 > "%skipIntro%"
+	echo 1 > "%skipConfirmation%"
+	echo 1 > "%disableCooldown%"
+)
+
+if %errorlevel% equ 7 (
 	if "%toggle1%" equ "%on%" del "%skipIntro%"
 	if "%toggle2%" equ "%on%" del "%skipConfirmation%"
 	if "%toggle3%" equ "%on%" del "%useAutoAlways%"
 	if "%toggle4%" equ "%on%" del "%useManualAlways%"
+	if "%toggle5%" equ "%on%" del "%disableCooldown%"
 )
 
-if %errorlevel% equ 7 goto:EOF
+if %errorlevel% equ 8 goto:EOF
 
 goto SETTINGS
